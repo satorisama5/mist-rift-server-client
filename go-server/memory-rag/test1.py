a3623c6849f4e7b5826f9f0c6252de1925afe0ea
sakanobo <1491571509@qq.com>
2026-03-07 10:59:03 +0800

    Delete go-server directory

--- go-server/memory-rag/test1.py
+++
@@ -1,9 +0,0 @@
-from qdrant_client import QdrantClient
-
-client = QdrantClient(
-    host="0.0.0.0",
-    port=6333,
-    prefer_grpc=False,
-    check_compatibility=False
-)
-print(client.get_collections())
